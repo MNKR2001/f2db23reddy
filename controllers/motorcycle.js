c8d750ae78fb1d38ffa7d8618c48db1c5a35fa1a
MNKR2001 <nkunalreddy2@gmail.com>
2022-11-10 19:46:15 -0600

    added  files

--- controllers/motorcycle.js
+++ controllers/motorcycle.js
@@ -3,7 +3,7 @@ var motorcycle = require('../models/motorcycle');
 exports.motorcycle_list = function(req, res) {
     res.send('NOT IMPLEMENTED: motorcycle list');
 };
-/ for a specific motorcycle. 
+//for a specific motorcycle. 
 exports.motorcycle_detail = async function(req, res) { 
     console.log("detail"  + req.params.id) 
     try { 
@@ -35,7 +35,6 @@ exports.motorcycle_create_post = async function(req, res) {
 exports.motorcycle_delete = function(req, res) {
     res.send('NOT IMPLEMENTED: motorcycle delete DELETE ' + req.params.id);
 };
-
 // Handle motorcycle update form on PUT.
 
 exports.motorcycle_update_put = function(req, res) {
@@ -52,6 +51,27 @@ exports.motorcycle_list = async function(req, res) {
         res.send(`{"error": ${err}}`);
     }  
 };
+
+//Handle motorcycle update form on PUT. 
+exports.motorcycle_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await motorcycle.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.model)  
+               toUpdate.model = req.body.model; 
+        if(req.body.cylindercapacity) toUpdate.cylindercapacity = req.body.cylindercapacity; 
+        if(req.body.colour) toUpdate.colour = req.body.colour; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
+}; 
 
 // VIEWS
 // Handle a show all view
