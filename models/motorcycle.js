e03adf261c38fdd333dbd98b566e2f131a58ec6e
MNKR2001 <nkunalreddy2@gmail.com>
2022-11-29 18:33:31 -0600

    made changes

--- models/motorcycle.js
+++ models/motorcycle.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose") 
 const motorcycleSchema = mongoose.Schema({ 
- model: String, 
- cylindercapacity: Number, 
- colour: String 
+ model: {type: String,required: [true, 'Name of the motorcycle cannot be empty']}, 
+ cylindercapacity: {type: Number,required: [true, 'cylindercapacity cannot be empty']}, 
+ colour: {type: String,required: [true, 'colour of the motorcycle cannot be empty']} 
 }) 
  
 module.exports = mongoose.model("motorcycle", 
--- routes/resource.js
+++ routes/resource.js
@@ -13,19 +13,35 @@ router.get('/', api_controller.api);
 
 /// University ROUTES ///
 
-// POST request for creating a University.
+// POST request for creating a Motorcycle.
 router.post('/motorcycle', motorcycle_controller.motorcycle_create_post);
 
-// PUT request to update University.
+// PUT request to update Motorcycle.
 router.put('/motorcycle/:id', motorcycle_controller.motorcycle_update_put);
-// GET request for one University.
+// GET request for one Motorcycle.
 router.get('/motorcycle/:id', motorcycle_controller.specific_motorcycle);
 
-// DELETE request to delete University.
+// DELETE request to delete Motorcycle.
 router.delete('/motorcycle/:id', motorcycle_controller.motorcycle_delete);
 
 
-// GET request for list of all University items.
+// GET request for list of all Motorcycle items.
 router.get('/motorcycle', motorcycle_controller.motorcycle_list);
 
+/* GET update Motorcycle page */ 
+router.get('/update', motorcycle_controller.motorcycle_update_Page); 
+
+// A little function to check if we have an authorized user and continue on 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
+
+/* GET update motorcycle page */ 
+router.get('/update', secured,    
+   motorcycle_controller.motorcycle_update_Page);   
 module.exports = router;
